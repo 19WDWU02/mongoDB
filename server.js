a7f3339863a74c471e330cfa889972d0b56fd4c5
Simon <watsonsemon@gmail.com>
2019-08-26 08:32:56 +1200

    changes

--- server.js
+++ server.js
@@ -2,8 +2,31 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+/* Broken on Yoobee Wifi */
+
+// const mongoose = require('mongoose');
+
+// const config = require('./config.json');
+//
+// mongoose.connect(`mongodb+srv://${config.mongoDBUser}:${config.mongoDBPassword}@simonscluster-yyrov.mongodb.net/shop?retryWrites=true&w=majority`, {useNewUrlParser: true});
+//
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', ()=> {
+//     // we're connected!
+//     console.log('conected');
+// });
+
+/* Borken ends */
 
 const allProducts = require('./data/products');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
 
 app.use((req, res, next)=> {
     console.log(`${req.method} request for ${req.url}`);
@@ -30,6 +53,76 @@ app.get('/product/id=:id', (req, res)=> {
     if (!check) {
         res.send('Product not found');
     }
+});
+
+/* Broken at yoobee wifi */
+
+// const Product = requre('./models/product');
+// const Contact = requre('./models/contact');
+
+/* Broken stops */
+
+app.post('/product/add', (req, res)=> {
+
+    /* Broken at yoobee wifi */
+
+    // const product = new Product({
+    //     _id: new mongoose.Types.ObjectId(),
+    //     name: req.body.name,
+    //     price: req.body.price
+    // });
+    //
+    // product.save().then((result)=> {
+    //     res.send(result);
+    // }).catch((err)=> {
+    //     res.send(err);
+    // });
+
+    /* Broken stops */
+
+    const newName = req.body.name;
+    const newPrice = parseFloat(req.body.price);
+
+    let newId = allProducts[allProducts.length - 1].id + 1;
+    let newData = {
+        id: newId,
+        name: newName,
+        price: newPrice
+    }
+    allProducts.push(newData);
+    let newDataString = JSON.stringify(allProducts);
+    fs.writeFileSync('data/products.json', newDataString);
+    res.send('Product Added');
+});
+
+app.post('/contact', (req, res)=> {
+    /* Broken at yoobee wifi */
+
+    const contact = new Contact({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        email: req.body.price,
+        message: req.body.message
+    });
+
+    contact.save().then((result)=> {
+        res.send(result);
+    }).catch((err)=> {
+        res.send(err);
+    });
+
+    /* Broken stops */
+});
+
+app.post('/user/add', (req, res)=> {
+    let username = req.body.username;
+    let password = req.body.password;
+    let age = req.body.age;
+    let favColor = req.body.favColor;
+
+    /* Add user to the database */
+
+    // res.send(newUser);
 });
 
 app.get('/product/delete/id=:id', (req, res)=> {
@@ -69,7 +162,7 @@ app.get('/product/edit/id=:id&name=:name&price=:price', (req, res)=> {
     });
     if (check) {
         let newDataString = JSON.stringify(newData);
-        fs.writeFileSync('data/products2.json', newDataString);
+        fs.writeFileSync('data/products.json', newDataString);
         res.send('Product edited');
     } else {
         res.send('No product was found');
